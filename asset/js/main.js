5791a3f664a18f21cd81208956c0ee1b7381548a
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-04-03 10:51:57 +0200

    clear cod for comment

--- asset/js/main.js
+++ asset/js/main.js
@@ -168,7 +168,7 @@ const app = createApp({
             ],
             activechat: 0,
             messageNew: '',
-            data : '',
+            data: '',
             newobjet: {
                 date: '',
                 message: '',
@@ -177,9 +177,9 @@ const app = createApp({
             datelastaccess: [],
             search: '',
             activeSearch: false,
-            showMenus : false,
-            lastAcces : '',
-            last_enter : ''
+            showMenus: false,
+            lastAcces: '',
+            last_enter: ''
         }
     },
     methods: {
@@ -189,9 +189,9 @@ const app = createApp({
         genereted_new_mess() {
             let now = this.datenow()
             let lastenter2 = String(now)
-            lastenter2 = lastenter2.substring(11,16)
-           
-            this.datelastaccess.splice(this.activechat,1,lastenter2);
+            lastenter2 = lastenter2.substring(11, 16)
+
+            this.datelastaccess.splice(this.activechat, 1, lastenter2);
             this.newobjet.date = now
             this.newobjet.message = this.messageNew
             this.contacts[this.activechat].messages.push({ ...this.newobjet });
@@ -208,115 +208,111 @@ const app = createApp({
             this.genereted_new_mess(index);
             setTimeout(() => { this.ask_new_mess() }, 1000);
         },
-        
-        
-    
-        
-        showmenu(){
+
+
+
+
+        showmenu() {
             this.showMenus = !this.showMenus
-            
+
         },
-        removemess(i){
-            this.contacts[this.activechat].messages.splice(i,1)
+        removemess(i) {
+            this.contacts[this.activechat].messages.splice(i, 1)
         },
-        logremove(i){
+        logremove(i) {
             console.log(i);
             console.log(this.contacts[this.activechat].messages);
             console.log(this.contacts[this.activechat].messages[i]);
         },
-        lastenter(){
-            this.contacts.forEach((contatto,index) => {
+        lastenter() {
+            this.contacts.forEach((contatto, index) => {
                 let position_last_mess = (contatto.messages.length) - 1
                 let lastenter2 = String(contatto.messages[position_last_mess].date)
-                lastenter2 = lastenter2.substring(11,16)
+                lastenter2 = lastenter2.substring(11, 16)
                 this.lastAcces = lastenter2
-                this.datelastaccess.splice(index,1,lastenter2)
+                this.datelastaccess.splice(index, 1, lastenter2)
                 return this.lastAcces
 
             })
         },
-        
-       
-        datenow(){
-            
+
+
+        datenow() {
+
             let giorno = new Date().getDate()
-            if(giorno <= 9 ){
+            if (giorno <= 9) {
                 giorno = '0' + giorno
             }
             let mese = (new Date().getMonth()) + 1
-            
+
             let anno = new Date().getFullYear()
             let ora = new Date().getHours()
             let minuti = new Date().getMinutes()
-            if(minuti <= 9 ){
+            if (minuti <= 9) {
                 minuti = '0' + minuti
             }
             return ` ${giorno}/0${mese}/${anno} ${ora}:${minuti}:00 `
-           
+
         },
-        showLastAccess(){
-            if(this.contacts[this.activechat].messages.length < 1){
+        showLastAccess() {
+            if (this.contacts[this.activechat].messages.length < 1) {
                 return this.datelastaccess[this.activechat]
-            }else{
+            } else {
                 return this.last_acces
             }
         },
-        showLastEnter(contatto,index){
-            if(contatto.messages.length < 1){
+        showLastEnter(contatto, index) {
+            if (contatto.messages.length < 1) {
                 return this.datelastaccess[index]
-            }else{
+            } else {
                 return this.lastenter(contatto)
             }
         },
-        last_acces(){
+        last_acces() {
             let position_last_mess = (this.contacts[this.activechat].messages.length) - 1
             let lastenter2 = String(this.contacts[this.activechat].messages[position_last_mess].date)
-            lastenter2 = lastenter2.substring(11,16)
-           
-            this.datelastaccess.splice(this.activechat,1,lastenter2)
+            lastenter2 = lastenter2.substring(11, 16)
+
+            this.datelastaccess.splice(this.activechat, 1, lastenter2)
             return lastenter2
         },
     },
-    
-    computed:{
+
+    computed: {
         search_name() {
-            if(this.search != '' && this.contacts[this.activechat].messages.length > 1){
+            if (this.search != '' && this.contacts[this.activechat].messages.length > 1) {
                 this.confronto()
-            
-            }else{
+
+            } else {
                 this.contacts.forEach(contatto => {
                     contatto.visible = true
-                    
+
                 })
             }
-            
+
         },
-        confronto(){
-            /* if(this.search != ''){ */
+        confronto() {
             this.contacts.forEach(contatto => {
                 if (contatto.name.toLowerCase().includes(this.search.toLowerCase())) {
                     console.log('la parola cercata si trova dentro array');
-                    console.log(contatto.name.toLowerCase().substring(0,this.search.length) + '  ' + contatto.name);
+                    console.log(contatto.name.toLowerCase().substring(0, this.search.length) + '  ' + contatto.name);
                     console.log(contatto.visible);
                     contatto.visible = true
                     console.log(contatto.visible);
-                }else{
-                    console.log(this.search.toLowerCase() + '=' + contatto.name.toLowerCase().substring(0,this.search.length) + '  ' + contatto.name);
+                } else {
+                    console.log(this.search.toLowerCase() + '=' + contatto.name.toLowerCase().substring(0, this.search.length) + '  ' + contatto.name);
                     console.log('non è uguale');
                     console.log(contatto.visible);
                     contatto.visible = false
                     console.log(contatto.visible);
                 }
             })
-            return true
-            /*} else{
-                return  false
-            } */
-    },
+
+        },
     },
     mounted() {
         this.last_acces(),
-        this.lastenter()
+            this.lastenter()
     }
 })
 
